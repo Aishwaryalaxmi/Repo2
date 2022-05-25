abe69a05f5d491a014f62f076b24d8beff1c86c8
Aishwarya <aishwaryalaxmi.u@ymedialabs.com>
2022-05-25 19:10:45 +0530

    Comitting th project

---
+++ TestSwiftPackageProject/TestSwiftPackageProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSwiftPackageProject
+//
+//  Created by Aishwarya laxmi U on 25/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
