056057b2243409121b998b20c8cf3c667eb863f4
Aishwaryalaxmi <Aishwaryalaxmi@users.noreply.github.com>
2022-06-07 10:41:10 +0000

    Swift format

--- TestSwiftPackageProject/TestSwiftPackageProjectTests/TestSwiftPackageProjectTests.swift
+++ TestSwiftPackageProject/TestSwiftPackageProjectTests/TestSwiftPackageProjectTests.swift
@@ -5,11 +5,10 @@
 //  Created by Aishwarya laxmi U on 25/05/22.
 //
 
-import XCTest
 @testable import TestSwiftPackageProject
+import XCTest
 
 class TestSwiftPackageProjectTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ class TestSwiftPackageProjectTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
